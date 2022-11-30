88b28d9ac0a8fd7ac44c85c696e8c9339434fbcd
xiaozhaofu <xiaozhaofu@163.com>
2022-11-30 16:53:50 +0800

    fix:优化范型类型

--- map.go
+++ map.go
@@ -1,11 +1,15 @@
 // @Author xiaozhaofu 2022/11/28 01:07:00
 package hase
 
+type intstring interface {
+	int | int8 | int16 | int32 | int64 | string
+}
+
 // Slice2Map 将字符串 slice 转为 map[T]struct{}。
 // 使用空结构体 struct{} 作为 value 的类型，因为 struct{} 不占用任何内存空间
 // fmt.Println(unsafe.Sizeof(bool(false))) // 1
 // fmt.Println(unsafe.Sizeof(struct{}{}))  // 0
-func Slice2Map[T int | int32 | int64 | string](sl []T) map[T]struct{} {
+func Slice2Map[T intstring](sl []T) map[T]struct{} {
 	set := make(map[T]struct{}, len(sl))
 	for _, v := range sl {
 		set[v] = struct{}{}
@@ -14,7 +18,7 @@ func Slice2Map[T int | int32 | int64 | string](sl []T) map[T]struct{} {
 }
 
 // InMap 判断字符串是否在 map 中。
-func InMap[T int | int32 | int64 | string](m map[T]struct{}, s T) bool {
+func InMap[T intstring](m map[T]struct{}, s T) bool {
 	_, ok := m[s]
 	return ok
 }
--- set_test.go
+++ set_test.go
@@ -5,22 +5,22 @@ import (
 	"testing"
 )
 
-// var sl = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+var sl = []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
 
-var sl = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+// var sl = []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 
 func BenchmarkInSlice(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		// InSlice(sl, "m")
-		InSlice(sl, 2)
+		InSlice(sl, "m")
+		// InSlice(sl, 2)
 	}
 }
 
 func BenchmarkInMap(b *testing.B) {
 	m := Slice2Map(sl)
 	for i := 0; i < b.N; i++ {
-		// InStrMap(m, "m")
-		InMap(m, 2)
+		InMap(m, "m")
+		// InMap(m, 2)
 	}
 }
 
